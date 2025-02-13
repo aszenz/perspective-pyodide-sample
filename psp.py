927eee6575eabd0f513775c830f76e828ec37f63
asrar <asrar@qbil.nl>
2025-02-13 15:11:56 +0100

    fix: Whl path when hosting under sub path

--- psp.py
+++ psp.py
@@ -16,9 +16,11 @@ app = marimo.App(width="full")
 @app.cell
 async def _():
     import sys 
+    import marimo as mo
     if "pyodide" in sys.modules:
         import micropip
-        await micropip.install("http:/public/perspective_python-3.3.4-cp39-abi3-emscripten_3_1_58_wasm32.whl")
+        whl_path = mo.notebook_location() / "public" / "perspective_python-3.3.4-cp39-abi3-emscripten_3_1_58_wasm32.whl"
+        await micropip.install(str(whl_path))
     import perspective
     psp_table = perspective.table("name,age\naszen,99")
     print(psp_table.schema())
